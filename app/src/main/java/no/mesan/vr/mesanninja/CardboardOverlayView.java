e1720a7b8aa37b15ecc076e99ba8efffab5d134b
Thomas Pettersen <tpettersen@gmail.com>
2015-09-14 21:55:27 +0200

    Added red floor

--- app/src/main/java/no/mesan/vr/mesanninja/CardboardOverlayView.java
+++ app/src/main/java/no/mesan/vr/mesanninja/CardboardOverlayView.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Google Inc. All Rights Reserved.
-
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package no.mesan.vr.mesanninja;
 
 import android.content.Context;
@@ -28,71 +12,74 @@ import android.widget.LinearLayout;
  * Contains two sub-views to provide a simple stereo HUD.
  */
 public class CardboardOverlayView extends LinearLayout {
-  private final CardboardOverlayEyeView leftView;
-  private final CardboardOverlayEyeView rightView;
-  private AlphaAnimation textFadeAnimation;
+    private final CardboardOverlayEyeView leftView;
+    private final CardboardOverlayEyeView rightView;
+    private AlphaAnimation textFadeAnimation;
 
-  public CardboardOverlayView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    setOrientation(HORIZONTAL);
+    public CardboardOverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(HORIZONTAL);
 
-    LayoutParams params = new LayoutParams(
-      LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1.0f);
-    params.setMargins(0, 0, 0, 0);
+        LayoutParams params = new LayoutParams(
+                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1.0f);
+        params.setMargins(0, 0, 0, 0);
 
-    leftView = new CardboardOverlayEyeView(context, attrs);
-    leftView.setLayoutParams(params);
-    addView(leftView);
+        leftView = new CardboardOverlayEyeView(context, attrs);
+        leftView.setLayoutParams(params);
+        addView(leftView);
 
-    rightView = new CardboardOverlayEyeView(context, attrs);
-    rightView.setLayoutParams(params);
-    addView(rightView);
+        rightView = new CardboardOverlayEyeView(context, attrs);
+        rightView.setLayoutParams(params);
+        addView(rightView);
 
-    // Set some reasonable defaults.
-    setDepthOffset(0.01f);
-    setColor(Color.rgb(150, 255, 180));
-    setVisibility(View.VISIBLE);
+        // Set some reasonable defaults.
+        setDepthOffset(0.01f);
+        setColor(Color.rgb(150, 255, 180));
+        setVisibility(View.VISIBLE);
 
-    textFadeAnimation = new AlphaAnimation(1.0f, 0.0f);
-    textFadeAnimation.setDuration(5000);
-  }
+        textFadeAnimation = new AlphaAnimation(1.0f, 0.0f);
+        textFadeAnimation.setDuration(5000);
+    }
 
-  public void show3DToast(String message) {
-    setText(message);
-    setTextAlpha(1f);
-    textFadeAnimation.setAnimationListener(new EndAnimationListener() {
-      @Override
-      public void onAnimationEnd(Animation animation) {
-        setTextAlpha(0f);
-      }
-    });
-    startAnimation(textFadeAnimation);
-  }
+    public void show3DToast(String message) {
+        setText(message);
+        setTextAlpha(1f);
+        textFadeAnimation.setAnimationListener(new EndAnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                setTextAlpha(0f);
+            }
+        });
+        startAnimation(textFadeAnimation);
+    }
 
-  private abstract class EndAnimationListener implements Animation.AnimationListener {
-    @Override
-    public void onAnimationRepeat(Animation animation) {}
-    @Override
-    public void onAnimationStart(Animation animation) {}
-  }
+    private void setDepthOffset(float offset) {
+        leftView.setOffset(offset);
+        rightView.setOffset(-offset);
+    }
 
-  private void setDepthOffset(float offset) {
-    leftView.setOffset(offset);
-    rightView.setOffset(-offset);
-  }
+    private void setText(String text) {
+        leftView.setText(text);
+        rightView.setText(text);
+    }
 
-  private void setText(String text) {
-    leftView.setText(text);
-    rightView.setText(text);
-  }
+    private void setTextAlpha(float alpha) {
+        leftView.setTextViewAlpha(alpha);
+        rightView.setTextViewAlpha(alpha);
+    }
 
-  private void setTextAlpha(float alpha) {
-    leftView.setTextViewAlpha(alpha);
-    rightView.setTextViewAlpha(alpha);
-  }
+    private void setColor(int color) {
+        leftView.setColor(color);
+        rightView.setColor(color);
+    }
 
-  private void setColor(int color) {
-    leftView.setColor(color);
-    rightView.setColor(color);
-  }
+    private abstract class EndAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+    }
 }
--- app/src/main/java/no/mesan/vr/mesanninja/MainActivity.java
+++ app/src/main/java/no/mesan/vr/mesanninja/MainActivity.java
@@ -1,7 +1,10 @@
 package no.mesan.vr.mesanninja;
 
+import android.opengl.GLES20;
+import android.opengl.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,24 +14,117 @@ import com.google.vrtoolkit.cardboard.Eye;
 import com.google.vrtoolkit.cardboard.HeadTransform;
 import com.google.vrtoolkit.cardboard.Viewport;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 
 public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer{
+
+    private static final String TAG = "MainActivity";
+
+    private CardboardOverlayView viewCardboardOverlay;
+
+    private static final int COORDS_PER_VERTEX = 3;
+    private static final float CAMERA_Z = 0.01f;
+    private static final float Z_NEAR = 0.1f;
+    private static final float Z_FAR = 100.0f;
+
+    // We keep the light always position just above the user.
+    private static final float[] LIGHT_POS_IN_WORLD_SPACE = new float[]{0.0f, 2.0f, 0.0f, 1.0f};
+
+    private int floorProgram;
+
+    private FloatBuffer floorVertices;
+    private FloatBuffer floorColors;
+    private FloatBuffer floorNormals;
+
+    private int floorPositionParam;
+    private int floorNormalParam;
+    private int floorColorParam;
+    private int floorModelParam;
+    private int floorModelViewParam;
+    private int floorModelViewProjectionParam;
+    private int floorLightPosParam;
+
+    private float[] modelView;
+    private float[] modelFloor;
+    private float[] modelViewProjection;
+    private float[] camera;
+    private float[] headView;
+    private float[] view;
+
+    private float floorDepth = 30f;
+    private final float[] lightPosInEyeSpace = new float[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initGui();
+        initValues();
+    }
+
+    private void initValues() {
+
+        modelView = new float[16];
+        modelViewProjection = new float[16];
+        modelFloor = new float[16];
+        camera = new float[16];
+        headView = new float[16];
+        view = new float[16];
+    }
+
+    private void initGui() {
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.viewCardboard);
+        cardboardView.setRestoreGLStateEnabled(false);
+        cardboardView.setRenderer(this);
+        setCardboardView(cardboardView);
+
+        viewCardboardOverlay = (CardboardOverlayView) findViewById(R.id.viewCardboardOverlay);
     }
 
     @Override
     public void onNewFrame(HeadTransform headTransform) {
 
+        // Set the background clear color to gray.
+        GLES20.glClearColor(1f, 1f, 1f, 1f);
+
+        // Build the camera matrix and apply it to the ModelView.
+        Matrix.setLookAtM(camera, 0, 0.0f, 0.0f, CAMERA_Z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+        headTransform.getHeadView(headView, 0);
+
+        checkGLError("onReadyToDraw");
     }
 
     @Override
     public void onDrawEye(Eye eye) {
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
+        checkGLError("colorParam");
+
+        // Apply the eye transformation to the camera.
+        Matrix.multiplyMM(view, 0, eye.getEyeView(), 0, camera, 0);
+
+        // Set the position of the light
+        Matrix.multiplyMV(lightPosInEyeSpace, 0, view, 0, LIGHT_POS_IN_WORLD_SPACE, 0);
+
+        float[] perspective = eye.getPerspective(Z_NEAR, Z_FAR);
+
+        // Set modelView for the floor, so we draw floor in the correct location
+        Matrix.multiplyMM(modelView, 0, view, 0, modelFloor, 0);
+        Matrix.multiplyMM(modelViewProjection, 0, perspective, 0,
+                modelView, 0);
+        drawFloor();
     }
 
     @Override
@@ -37,17 +133,168 @@ public class MainActivity extends CardboardActivity implements CardboardView.Ste
     }
 
     @Override
-    public void onSurfaceChanged(int i, int i1) {
-
+    public void onSurfaceChanged(int width, int height) {
+        Log.i(TAG, "onSurfaceChanged");
     }
 
     @Override
     public void onSurfaceCreated(EGLConfig eglConfig) {
+        Log.i(TAG, "onSurfaceCreated");
+
+        // Set the background clear color to gray.
+        GLES20.glClearColor(1f, 1f, 1f, 1f);
+
+        int vertexShader = loadGLShader(GLES20.GL_VERTEX_SHADER, R.raw.light_vertex);
+        int gridShader = loadGLShader(GLES20.GL_FRAGMENT_SHADER, R.raw.grid_fragment);
+
+        // make a floor
+        ByteBuffer bbFloorVertices = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_COORDS.length * 4);
+        bbFloorVertices.order(ByteOrder.nativeOrder());
+        floorVertices = bbFloorVertices.asFloatBuffer();
+        floorVertices.put(WorldLayoutData.FLOOR_COORDS);
+        floorVertices.position(0);
+
+        ByteBuffer bbFloorNormals = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_NORMALS.length * 4);
+        bbFloorNormals.order(ByteOrder.nativeOrder());
+        floorNormals = bbFloorNormals.asFloatBuffer();
+        floorNormals.put(WorldLayoutData.FLOOR_NORMALS);
+        floorNormals.position(0);
+
+        ByteBuffer bbFloorColors = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_COLORS.length * 4);
+        bbFloorColors.order(ByteOrder.nativeOrder());
+        floorColors = bbFloorColors.asFloatBuffer();
+        floorColors.put(WorldLayoutData.FLOOR_COLORS);
+        floorColors.position(0);
+
+        floorProgram = GLES20.glCreateProgram();
+        GLES20.glAttachShader(floorProgram, vertexShader);
+        GLES20.glAttachShader(floorProgram, gridShader);
+        GLES20.glLinkProgram(floorProgram);
+        GLES20.glUseProgram(floorProgram);
+
+        checkGLError("Floor program");
+
+        floorModelParam = GLES20.glGetUniformLocation(floorProgram, "u_Model");
+        floorModelViewParam = GLES20.glGetUniformLocation(floorProgram, "u_MVMatrix");
+        floorModelViewProjectionParam = GLES20.glGetUniformLocation(floorProgram, "u_MVP");
+        floorLightPosParam = GLES20.glGetUniformLocation(floorProgram, "u_LightPos");
+
+        floorPositionParam = GLES20.glGetAttribLocation(floorProgram, "a_Position");
+        floorNormalParam = GLES20.glGetAttribLocation(floorProgram, "a_Normal");
+        floorColorParam = GLES20.glGetAttribLocation(floorProgram, "a_Color");
+
+        GLES20.glEnableVertexAttribArray(floorPositionParam);
+        GLES20.glEnableVertexAttribArray(floorNormalParam);
+        GLES20.glEnableVertexAttribArray(floorColorParam);
+
+        checkGLError("Floor program params");
+
+        Matrix.setIdentityM(modelFloor, 0);
+        Matrix.translateM(modelFloor, 0, 0, -floorDepth, 0); // Floor appears below user.
+
+        checkGLError("onSurfaceCreated");
 
     }
 
     @Override
     public void onRendererShutdown() {
+        Log.i(TAG, "onRendererShutdown");
+    }
 
+
+
+    /**
+     * Draw the floor.
+     * <p/>
+     * <p>This feeds in data for the floor into the shader. Note that this doesn't feed in data about
+     * position of the light, so if we rewrite our code to draw the floor first, the lighting might
+     * look strange.
+     */
+    public void drawFloor() {
+        GLES20.glUseProgram(floorProgram);
+
+        // Set ModelView, MVP, position, normals, and color.
+        GLES20.glUniform3fv(floorLightPosParam, 1, lightPosInEyeSpace, 0);
+        GLES20.glUniformMatrix4fv(floorModelParam, 1, false, modelFloor, 0);
+        GLES20.glUniformMatrix4fv(floorModelViewParam, 1, false, modelView, 0);
+        GLES20.glUniformMatrix4fv(floorModelViewProjectionParam, 1, false,
+                modelViewProjection, 0);
+        GLES20.glVertexAttribPointer(floorPositionParam, COORDS_PER_VERTEX, GLES20.GL_FLOAT,
+                false, 0, floorVertices);
+        GLES20.glVertexAttribPointer(floorNormalParam, 3, GLES20.GL_FLOAT, false, 0,
+                floorNormals);
+        GLES20.glVertexAttribPointer(floorColorParam, 4, GLES20.GL_FLOAT, false, 0, floorColors);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6);
+
+        checkGLError("drawing floor");
+    }
+
+
+    /**
+     * Checks if we've had an error inside of OpenGL ES, and if so what that error is.
+     *
+     * @param label Label to report in case of error.
+     */
+    private static void checkGLError(String label) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, label + ": glError " + error);
+            throw new RuntimeException(label + ": glError " + error);
+        }
+    }
+
+    /**
+     * Converts a raw text file, saved as a resource, into an OpenGL ES shader.
+     *
+     * @param type  The type of shader we will be creating.
+     * @param resId The resource ID of the raw text file about to be turned into a shader.
+     * @return The shader object handler.
+     */
+    private int loadGLShader(int type, int resId) {
+        String code = readRawTextFile(resId);
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, code);
+        GLES20.glCompileShader(shader);
+
+        // Get the compilation status.
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0) {
+            Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+            GLES20.glDeleteShader(shader);
+            shader = 0;
+        }
+
+        if (shader == 0) {
+            throw new RuntimeException("Error creating shader.");
+        }
+
+        return shader;
+    }
+
+    /**
+     * Converts a raw text file into a string.
+     *
+     * @param resId The resource ID of the raw text file about to be turned into a shader.
+     * @return The context of the text file, or null in case of error.
+     */
+    private String readRawTextFile(int resId) {
+        InputStream inputStream = getResources().openRawResource(resId);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            reader.close();
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
